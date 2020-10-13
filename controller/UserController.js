c8ac88cc920bd15a44a299fbe2b1e13a4270886b
Thulio Barbosa <64238577+thuliobarbosa@users.noreply.github.com>
2020-10-13 17:25:22 -0300

    New changes

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,14 +1,56 @@
 class UserController {
 
-    constructor(formId,tableId) {
+    constructor(formIdCreate, formIdUpdate, tableId) {
 
-        this.formElement = document.getElementById(formId);
+        this.formElement = document.getElementById(formIdCreate);
+        this.formUpdateElement = document.getElementById(formIdUpdate);
         this.tableElement = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEdit();
 
     }
 
+    onEdit() {
+        document.querySelector("#box-user-update .btn-cancel").addEventListener('click', (e) => {
+            this.showPanelCreate();
+        });
+
+        this.formUpdateElement.addEventListener('submit', (event) => {
+
+            event.preventDefault();
+
+            let btn = this.formUpdateElement.querySelector("[type=submit]");
+            
+            btn.disabled = true;
+
+            let values = this.getValues(this.formUpdateElement);
+
+            let index = this.formUpdateElement.dataset.trIndex;
+
+            let tr = this.tableElement.rows[index]; 
+
+            tr.dataset.user = JSON.stringify(values);
+
+            tr.innerHTML = `
+
+                <td><img src="${values.photo}" alt="User Image" class="img-circle img-sm"></td>
+                <td>${values.name}</td>
+                <td>${values.email}</td>
+                <td>${(values.admin) ? 'Sim' : 'Não'}</td>
+                <td>${Utils.dateFormat(values.register)}</td>
+                <td>
+                <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td> 
+            `;
+
+            this.addEventsTR(tr);
+
+            this.updateCount();
+
+        });
+    }
 
     onSubmit() {
 
@@ -16,12 +58,14 @@ class UserController {
         
             event.preventDefault();
 
-            btn = this.formElement.querySelector("[type=submit]");
+            let btn = this.formElement.querySelector("[type=submit]");
             
             btn.disabled = true;
 
-            let values = this.getValues();
+            let values = this.getValues(this.formElement);
             
+            if (!values) return false;
+
             this.getPhoto().then(
                 
                 (content) => {
@@ -86,12 +130,20 @@ class UserController {
     };
 
 
-    getValues() {
+    getValues(formElement) {
 
         let user = {};
+        let isValid = true;
 
-        [...this.formElement.elements].forEach((field, index) => {
+        [...formElement.elements].forEach((field, index) => {
 
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && ! field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
+        
             if (field.name === 'gender') {
         
                 if (field.checked) {
@@ -107,6 +159,10 @@ class UserController {
             }
         
         });
+
+        if (! isValid){
+            return false;
+        };
     
         return new User(
             user.name,
@@ -124,7 +180,9 @@ class UserController {
 
     addLine(dataUser) {
 
-        let tr = document.createElement('tr');
+        let tr = document.createElement('tr'); 
+
+        tr.dataset.user = JSON.stringify(dataUser);
 
         tr.innerHTML = `
 
@@ -132,15 +190,87 @@ class UserController {
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-            <td>${dataUser.birth}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td> 
         `;
+    
+        this.addEventsTR(tr);
 
         this.tableElement.appendChild(tr);
-    
+
+        this.updateCount();
+
+    };
+
+    addEventsTR(tr) {
+        tr.querySelector(".btn-edit").addEventListener('click', (e) => {
+            
+            let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector("#form-user-update");
+
+            form.dataset.trIndex = tr.sectionRowIndex;
+
+            for (let name in json) {
+
+                let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+
+                if (field){
+
+                    switch (field.type) {
+                        case 'file':
+                            continue;
+                        break;
+                        case 'radio':
+                            field = form.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]");
+                            field.checked = true;
+                        break;
+                        case 'checkbox':
+                            field.checked = json[name];
+                        break;
+
+                        default:
+                            field.value = json[name];
+                    }
+                    
+                }
+
+            }
+           
+            this.showPanelUpdate();
+
+        });
+
+    }
+
+    showPanelCreate() {
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+    }
+
+    showPanelUpdate() {
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
+    }
+
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableElement.children].forEach( (tr) => {
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) numberAdmin++;
+
+        })
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
     };
 
 };
--- model/User.js
+++ model/User.js
@@ -2,17 +2,87 @@ class User {
 
     constructor(name, gender, birth, country, email, password, photo, admin) {
 
-        this.name = name;
-        this.gender = gender;
-        this.birth = birth;
-        this.country = country;
-        this.email = email;
-        this.password = password;
-        this.photo = photo;
-        this.admin = admin;
+        this._name = name;
+        this._gender = gender;
+        this._birth = birth;
+        this._country = country;
+        this._email = email;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
+        this._register = new Date();
 
     }
 
+    get register() {
+        return this._register;
+    }
 
+    set register(value) {
+        this._register = value;
+    }
 
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(value) {
+        this._gender = value;
+    }
+
+    get birth() {
+        return this._birth;
+    }
+
+    set birth(value) {
+        this._birth = value;
+    }
+
+    get country(){
+        return this._country;
+    }
+
+    set country(value) {
+        this._country = value;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(value) {
+        this._email = value;
+    }
+
+    get password() {
+        return this._password
+    }
+
+    set password(value) {
+        this._password = value;
+    }
+
+    get photo(){
+        return this._photo;
+    }
+
+    set photo(value) {  
+        this._photo = value;
+    }
+
+    get admin() {
+        return this._admin;
+    }
+
+    set admin(value) {
+        this._admin = value;
+    }
 }
